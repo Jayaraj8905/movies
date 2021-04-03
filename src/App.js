06e078ecbcaa3b44a5228759c91cfa11101c6e63
Jayaraj <jayaraj8905@gmail.com>
2021-04-03 17:07:19 +0800

    Implementation of adding favourites

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import MovieSearch from './movies/search';
 // http://www.omdbapi.com/?i=tt3896198&apikey=4d288a4e
 const App = () =>  {
   const [movies, setMovies] = useState([]);
+  const [favourites, setFavourites] = useState([]);
   const [searchStr, setSearchStr] = useState('');
   const apikey = '4d288a4e';
 
@@ -23,13 +24,57 @@ const App = () =>  {
     getMovies();
   }, [searchStr]);
 
+  useEffect(() => {
+		const movieFavourites = JSON.parse(
+			localStorage.getItem('movies-favourites')
+		);
+
+		if (movieFavourites) {
+			setFavourites(movieFavourites);
+		}
+	}, []);
+
+  const saveToLocalStorage = (items) => {
+		localStorage.setItem('movies-favourites', JSON.stringify(items));
+	};
+
+	const addFavouriteMovie = (movie) => {
+    const alreadyExists = favourites.find((favouriteMovie) => favouriteMovie.imdbID === movie.imdbID);
+    if (!alreadyExists) {
+      const newFavouriteList = [...favourites, movie];
+      setFavourites(newFavouriteList);
+      saveToLocalStorage(newFavouriteList);
+    }
+	};
+
+	const removeFavouriteMovie = (movie) => {
+		const newFavouriteList = favourites.filter(
+			(favourite) => favourite.imdbID !== movie.imdbID
+		);
+
+		setFavourites(newFavouriteList);
+		saveToLocalStorage(newFavouriteList);
+	};
+
   return (
     <div className="movies container-fluid">
       <div className='row d-flex align-items-center mt-4 mb-4'>
+        <div className='col'>
+          <h3>Movies</h3>
+        </div>
 				<MovieSearch value={searchStr} setSearchStr={setSearchStr} />
 			</div>
       <div className="row">
-        <MoviesList movies={movies}></MoviesList>
+        <MoviesList movies={movies} handleFavouritesClick={addFavouriteMovie} addFavourites={true}></MoviesList>
+      </div>
+
+      <div className='row d-flex align-items-center mt-4 mb-4'>
+        <div className='col'>
+          <h3>Favourites</h3>
+        </div>
+			</div>
+      <div className="row">
+        <MoviesList movies={favourites} handleFavouritesClick={removeFavouriteMovie} addFavourites={false}></MoviesList>
       </div>
     </div>
   );
--- src/movies/list.js
+++ src/movies/list.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { HeartFill, HeartHalf } from 'react-bootstrap-icons';
 
 const MoviesList = (props) => {
+    const Component = props.addFavourites ? HeartFill : HeartHalf;
     return (
         <>
-            {props.movies.map((movie, index) => (
-                <div className='image-holder d-flex align-items-start justify-content-center m-3' key={movie.imdbID}>
+            {props.movies.map((movie, index) => {
+                return <div className='image-holder d-flex align-items-start justify-content-center m-3' key={movie.imdbID}>
                     <img src={movie.Poster} alt='Not Available'></img>
+                    <div
+						onClick={() => props.handleFavouritesClick(movie)}
+						className='overlay d-flex align-items-center justify-content-center'
+					>
+                        <Component color="red"/>
+					</div>
+                    
                 </div>
-            ))}
+            })}
         </>
     )
 };
